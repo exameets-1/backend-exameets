64929ddd9edc9fd0f0ea4986908fa03ee50d7adc
santhosh-2504 <santhoshanantha58@gmail.com>
2025-07-01 12:18:42 +0530

    Added domain option to cookie

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -17,6 +17,26 @@
 //      });
 // };
 
+// export const sendToken = (user, statusCode, res, message) => {
+//   const token = user.getJWTToken();
+//   const options = {
+//     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === 'production',
+//     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+//     path: '/',
+//   };
+
+//   res
+//     .status(statusCode)
+//     .cookie("token", token, options)
+//     .json({
+//       success: true,
+//       user,
+//       message,
+//     });
+// };
+
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
   const options = {
@@ -25,6 +45,7 @@ export const sendToken = (user, statusCode, res, message) => {
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     path: '/',
+    domain: process.env.NODE_ENV === 'production' ? '.exameets.in' : undefined,
   };
 
   res
@@ -36,3 +57,4 @@ export const sendToken = (user, statusCode, res, message) => {
       message,
     });
 };
+
