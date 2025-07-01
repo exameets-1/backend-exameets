d39ae593e3d084b930acbe1cd33405d0192342b0
santhosh-2504 <santhoshanantha58@gmail.com>
2025-07-01 11:56:45 +0530

    Token changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,18 +1,38 @@
+// export const sendToken = (user, statusCode, res, message) => {
+//   const token = user.getJWTToken();
+//   const options = {
+//      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+//      httpOnly: true,
+//      secure: process.env.NODE_ENV === 'production',
+//      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+//      path: '/'
+//    };
+  
+//   res.status(statusCode)
+//      .cookie("token", token, options)
+//      .json({
+//           success: true,
+//           user,
+//           message,
+//      });
+// };
+
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
   const options = {
-     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-     httpOnly: true,
-     secure: process.env.NODE_ENV === 'production',
-     sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
-     path: '/'
-   };
-  
-  res.status(statusCode)
-     .cookie("token", token, options)
-     .json({
-          success: true,
-          user,
-          message,
-     });
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+    path: '/',
+  };
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      user,
+      message,
+    });
 };
